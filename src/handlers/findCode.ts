56d28d0c53b365707c5120dfab4675464a33d26b
CamiKaseM7 <camilorivasramunni@gmail.com>
2023-07-05 17:21:48 -0300

    Remove unused code

--- src/handlers/findCode.ts
+++ src/handlers/findCode.ts
@@ -1,9 +1,7 @@
 import { Message } from "discord.js";
 import Ags from "../commands/Utility/Ags";
-import { client } from "..";
 
 export async function findCode(message: Message<boolean>) {
-    if (message.author.id == client.user!.id) return;
     const words = message.content.split("\n").map(line => line.replaceAll(":", " ").replaceAll(">", " ").replaceAll("*", "").replaceAll("<", " ").split(" ")).flat();
     const regex = new RegExp(/([A-Z]+[a-z]+[A-Z]+)|([a-z-A-Z]+![0-9]+)/);
     const filteredWords = words.filter(word => regex.test(word));
