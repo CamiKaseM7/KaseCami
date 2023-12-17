0d33e2125c6cb0e48320051852075682ff6fce91
CamiKaseM7 <camilorivasramunni@gmail.com>
2023-12-17 14:44:33 -0300

    Log db connection attemtp

--- src/database/connectToDatabase.ts
+++ src/database/connectToDatabase.ts
@@ -3,6 +3,7 @@ import { connect } from "mongoose";
 const MONGODB_URI = "mongodb://localhost:27017/mydatabase"; // Replace with your own database URI
 
 export const connectToDatabase = async () => {
+    console.log("Connecting to MongoDB...");
     try {
         await connect(MONGODB_URI);
         console.log("Connected to MongoDB");
