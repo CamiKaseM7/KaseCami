f816dcd9a034cef6013f5fdc4f21051cc0d5600d
CamiKaseM7 <camilorivasramunni@gmail.com>
2024-01-18 21:53:54 -0300

    Get mongo server from env

--- src/database/connectToDatabase.ts
+++ src/database/connectToDatabase.ts
@@ -1,11 +1,11 @@
 import { connect } from "mongoose";
-
-const MONGODB_URI = "mongodb://localhost:27017/mydatabase"; // Replace with your own database URI
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectToDatabase = async () => {
     console.log("Connecting to MongoDB...");
     try {
-        await connect(MONGODB_URI);
+        await connect(process.env.MONGODB_URI!);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
